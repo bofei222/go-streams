9f9a6983c061700de225fafc8dff592f920218da
bofei <bofei222@163.com>
2024-07-02 21:58:12 +0800

    窗口，流

--- examples/bf/main.go
+++ examples/bf/main.go
@@ -40,35 +40,43 @@ func main() {
 		func(m map[string]interface{}) int64 {
 			return m["eventTime"].(int64)
 		})
-	slidingWindowH := flow.NewSlidingWindowWithExtractor[map[string]interface{}](
-		10000*time.Millisecond,
-		10000*time.Millisecond,
-		func(m map[string]interface{}) int64 {
-			return m["eventTime"].(int64)
-		})
-	//	return string(msg.Value)
-	//}
+	//slidingWindowH := flow.NewSlidingWindowWithExtractor[map[string]interface{}](
+	//	10000*time.Millisecond,
+	//	10000*time.Millisecond,
+	//	func(m map[string]interface{}) int64 {
+	//		return m["eventTime"].(int64)
+	//	})
 	//fileSink := ex.NewFileSink("out.txt")
 	out := make(chan any, 1000)
 	chanSink := ex.NewChanSink(out)
 	//stdoutSink := ex.NewStdoutSink()
 	xxFlow := flow.NewMap(xx, 1)
 	yyFlow := flow.NewMap(yyMap, 1)
+	//avgMet1sWSpd6Flow := flow.NewMap(calculateAverageMet1sWSpd6, 1)
 
-	source.
-		Via(flow.NewPassThrough()).
-		Via(parseJsonFlow).
-		Via(slidingWindow).
-		Via(xxFlow).
-		Via(yyFlow).
-		To(chanSink)
+	go func() {
 
-	chanSource := ex.NewChanSource(chanSink.Out)
-	chanSource.
-		Via(flow.NewPassThrough()).
-		Via(slidingWindowH).
-		To(ex.NewStdoutSink())
+		source.
+			Via(flow.NewPassThrough()).
+			Via(parseJsonFlow).
+			Via(slidingWindow).
+			Via(xxFlow).
+			Via(yyFlow).
 
+			//Via(avgMet1sWSpd6Flow).
+			To(chanSink)
+	}()
+
+	go func() {
+		newSource := ex.NewChanSource(out)
+		newSource.Via(xxFlow)
+
+		//for e := range out {
+		//	fmt.Println(e)
+		//}
+	}()
+
+	select {}
 }
 
 var parseJson = func(msg *sarama.ConsumerMessage) map[string]interface{} {
@@ -112,6 +120,23 @@ var yyMap = func(msg []map[string]interface{}) map[string]map[string]interface{}
 		//m["innerTurbineName"] = strconv.FormatFloat(m["innerTurbineName"].(float64), 'f', -1, 64)
 		result[m["innerTurbineName"].(string)] = m
 	}
-	fmt.Println(result)
+	//fmt.Println(result)
+	return result
+}
+
+var calculateAverageMet1sWSpd6 = func(msg map[string]map[string]interface{}) map[string]interface{} {
+	sum := 0.0
+	count := 0.0
+	for _, m := range msg {
+		if val, ok := m["Met1sWSpd6"].(float64); ok {
+			sum += val
+			count++
+		}
+	}
+	average := sum / count
+	result := map[string]interface{}{
+		"averageMet1sWSpd6": average,
+	}
+	fmt.Printf("Average Met1sWSpd6: %f\n", average)
 	return result
 }
--- examples/kafka/main.go
+++ examples/kafka/main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/IBM/sarama"
+	ex "github.com/reugn/go-streams/extension"
 	"github.com/reugn/go-streams/flow"
 	ext "github.com/reugn/go-streams/kafka"
 )
@@ -15,13 +16,13 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
 	defer cancel()
 
-	hosts := []string{"127.0.0.1:9092"}
+	hosts := []string{"10.19.8.243:9092"}
 	config := sarama.NewConfig()
 	config.Consumer.Group.Rebalance.Strategy = sarama.NewBalanceStrategyRoundRobin()
-	config.Consumer.Offsets.Initial = sarama.OffsetNewest
+	config.Consumer.Offsets.Initial = sarama.OffsetOldest
 	config.Producer.Return.Successes = true
 	config.Version, _ = sarama.ParseKafkaVersion("3.6.2")
-	groupID := "testConsumer"
+	groupID := "testConsumer3"
 
 	source, err := ext.NewKafkaSource(ctx, hosts, groupID, config, "test")
 	if err != nil {
@@ -30,12 +31,14 @@ func main() {
 
 	toUpperMapFlow := flow.NewMap(toUpper, 1)
 	appendAsteriskFlatMapFlow := flow.NewFlatMap(appendAsterisk, 1)
-	sink, err := ext.NewKafkaSink(hosts, config, "test2")
+	//appendAsteriskFlatMapFlow := flow.NewMap(appendAsterisk, 1)
+	//sink, err := ext.NewKafkaSink(hosts, config, "test2")
+	stdoutSink := ex.NewStdoutSink()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	throttler := flow.NewThrottler(1, time.Second, 50, flow.Discard)
+	throttler := flow.NewThrottler(100, time.Second, 3*100, flow.Discard)
 	tumblingWindow := flow.NewTumblingWindow[*sarama.ConsumerMessage](time.Second * 5)
 
 	source.
@@ -43,7 +46,7 @@ func main() {
 		Via(throttler).
 		Via(tumblingWindow).
 		Via(appendAsteriskFlatMapFlow).
-		To(sink)
+		To(stdoutSink)
 }
 
 var toUpper = func(msg *sarama.ConsumerMessage) *sarama.ConsumerMessage {
